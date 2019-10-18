af72865932dc55befc04d06f882e0bc4997de557
thorhalbert <thorhalbert@gmail.com>
2019-10-17 17:49:04 -0700

    Remove the plugins via the .gitignore I need a README on how you would load up the plugins. I do have gRPC for unity loaded up in plugins.

--- Assets/Code/ProcessElements/ProcessEmissary.cs
+++ Assets/Code/ProcessElements/ProcessEmissary.cs
@@ -10,11 +10,21 @@ namespace VRWorlds.Browser
     public class ProcessEmissary: ProcessBase
     {
         public Guid ProcessorRole { get; set; }
+        public Uri GrpcIngressUri { get; private set; }
+
+        public ProcessEmissary(): base()
+        {
+
+        }
 
         protected override void AugmentArguments(StringBuilder sb)
         {
             sb.Append("--processor-role ");
             sb.Append(ProcessorRole.ToString());
+            sb.Append(' ');
+
+            sb.Append("--grpc-ingress-uri ");
+            sb.Append(GrpcIngressUri.ToString());
             sb.Append(' ');
         }
 
