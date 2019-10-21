30baf201daaa11ecf7decafc2660bb31e906ca11
thorhalbert <thorhalbert@gmail.com>
2019-10-20 22:21:22 -0700

    Tried things with the Vive...  Starting to work. Now need to build the avatar controller as well as getting the GRPC stuff to work so we can call the emissary controllers.

--- Assets/Code/ProcessManager.cs
+++ Assets/Code/ProcessManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRWorlds.Browser;
 
 public class ProcessManager : MonoBehaviour
 {
+    private static List<ProcessBase> _processList = new List<ProcessBase>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // We're a master contoller
+        DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
