16ecfdbdcbb24acaebfff399abb9edc39d5f62ed
thorhalbert <thorhalbert@gmail.com>
2020-02-09 21:55:50 -0800

    Update SteamVR. Also start on the actual AvatarController. Need new input system.

--- Assets/Code/AvatarController.cs
+++ Assets/Code/AvatarController.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AvatarController : MonoBehaviour
+namespace VRWorlds.Browser.Avatar
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AvatarController : MonoBehaviour
     {
-        // We're a master contoller
-        DontDestroyOnLoad(this);
+        public float CharacterHeight = 1.8f;
+        public float EyeHeight = 1.8f - .1f;
+        public GameObject CameraRig = null;
 
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            var trans = CameraRig.GetComponent<Transform>();
+            trans.transform.SetPositionAndRotation(new Vector3(0, EyeHeight), new Quaternion());
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
